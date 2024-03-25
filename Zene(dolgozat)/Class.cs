7db2cf515aa80235620b44a622d2e67fc67e0684
diak <diak@diak.hu>
2024-03-25 15:20:11 +0100

    Add project files.

---
+++ Zene(dolgozat)/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zene_dolgozat_
+{
+    public class Class
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+        public int Length { get; set; }
+
+        public Class(string title, string artist, int year, int length)
+        {
+            Title = title;
+            Artist = artist;
+            Year = year;
+            Length = length;
+        }
+
+        public override string? ToString()
+        {
+            return $"Cím:{Title} Előadó: {Artist} Év: {Year} Hossz: {Length} seconds";
+        }
+    }
+}
---
+++ Zene(dolgozat)/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using Zene_dolgozat_;
+
+Class song = new Class("Never Gonna Give You Up", "Rick Astley", 1988, 212);
+Console.WriteLine(song);
